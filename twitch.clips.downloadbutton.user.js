f9a2737535fa9c40a4f48a043a1030d7855e35e1
Mark Harvey <crimson.falconer@gmail.com>
2018-06-10 20:40:26 +1000

    Move to use

--- twitch.clips.downloadbutton.user.js
+++ twitch.clips.downloadbutton.user.js
@@ -8,8 +8,8 @@
 // @include     https://player.twitch.tv/*
 // @updateURL       https://github.com/crimsonfalconer/twitch.clips.downloadbutton/raw/master/twitch.clips.downloadbutton.user.js
 // @downloadURL     https://github.com/crimsonfalconer/twitch.clips.downloadbutton/raw/master/twitch.clips.downloadbutton.user.js
-// @version     11
-// @updateVersion   11
+// @version     12
+// @updateVersion   12
 // ==/UserScript==
 
 var chk = 0;
@@ -18,7 +18,9 @@ var chk = 0;
 document.onkeypress = function (e) {
     e = e || window.event;
     if(e.key == 'd' && event.altKey && event.shiftKey){
-        top.window.location.href = document.getElementsByTagName('video')[0].src
+        var downloadURL = document.getElementsByTagName('video')[0].src.replace('-360.mp4', '.mp4').replace('AT-vod','vod').replace('640x360.mp4','.mp4').replace('-480.mp4','.mp4').replace('-720.mp4','.mp4');
+        if(downloadURL.indexOf('-offset') !== -1){downloadURL = downloadURL.replace('AT-cm%7C', '')}
+        top.window.location.href = downloadURL;
     }
 };
 
@@ -27,14 +29,20 @@ function CreateDownloadButton()
 {
   var panel = document.getElementsByClassName('player-buttons-right')[0];
   var save = document.createElement('button');
+  var link = document.createElement('a');
+  // Various Link Fixes to Source Quality
+  var downloadURL = document.getElementsByTagName('video')[0].src.replace('-360.mp4', '.mp4').replace('AT-vod','vod').replace('640x360.mp4','.mp4').replace('-480.mp4','.mp4').replace('-720.mp4','.mp4');
+  if(downloadURL.indexOf('-offset') !== -1){downloadURL = downloadURL.replace('AT-cm%7C', '')}
+  link.href = downloadURL;
   save.classList.add('player-button');
   save.type = 'button';
   save.value = 'Download';
-  save.onclick = function(){top.window.location.href = document.getElementsByTagName('video')[0].src};
+  //save.onclick = function(){top.window.location.href = };
   var btnSpn = document.createElement('span');
   btnSpn.setAttribute('style', 'color:#ffffff;font-size:20px')
   btnSpn.append('D');
-  save.appendChild(btnSpn);
+  link.append(btnSpn);
+  save.appendChild(link);
   panel.insertBefore(save, panel.firstChild);
 }
 
@@ -52,4 +60,4 @@ var i = setInterval(function() {
         clearInterval(i)
     }
     return
-}, 250);
+}, 1500);
